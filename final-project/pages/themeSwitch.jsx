ddeb83ccba6e579e0b2bb644062ac30a03680e20
ajboots <ajboots@ncsu.edu>
2023-03-03 10:05:30 -0500

    small renaming

--- final-project/pages/themeSwitch.jsx
+++ final-project/pages/themeSwitch.jsx
@@ -2,28 +2,28 @@ import { useState } from "react";
 import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 
 const ThemeSwitcher = () => {
-    const [theme, setTheme] = useState(null);
+    const [theme, setText] = useState(null);
 
-    const resetTheme = () => {
-        setTheme(null);
+    const resetText = () => {
+        setText(null);
     };
 
     return (
         <div className="mb-2">
 
-                <Button onClick={() => setTheme("YOU ARE WRONG AND SHOULD FEEL BAD")}>
+                <Button onClick={() => setText("YOU ARE WRONG AND SHOULD FEEL BAD")}>
                 &nbsp;&nbsp;1&nbsp;&nbsp;
                 </Button>
                 &nbsp;&nbsp;
-                <Button onClick={() => setTheme("YOU ARE WRONG AND SHOULD FEEL BAD")}>
+                <Button onClick={() => setText("YOU ARE WRONG AND SHOULD FEEL BAD")}>
                 &nbsp;&nbsp;2&nbsp;&nbsp;
                 </Button>
                 &nbsp;&nbsp;
-                <Button onClick={() => setTheme("YOU ARE {(right)} AND {(should not)} FEEL BAD")}>
+                <Button onClick={() => setText("YOU ARE {(right)} AND {(should not)} FEEL BAD")}>
                 &nbsp;&nbsp;3&nbsp;&nbsp;
                 </Button>
                 &nbsp;&nbsp;
-                <Button onClick={() => setTheme("YOU ARE WRONG AND SHOULD FEEL BAD")}>
+                <Button onClick={() => setText("YOU ARE WRONG AND SHOULD FEEL BAD")}>
                 &nbsp;&nbsp;4&nbsp;&nbsp;
                 </Button>                
                 <p>                   
